2df1320350714af8386d9e0612467d9436c3f00a
jglrxavpok <jglrxavpok@gmail.com>
2018-11-18 04:27:56 +0100

    Commande de l'ascenseur + commentaires

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,8 @@
 
 #define _GLIBCXX_USE_C99 1
 
+
+// Liste des moteurs du bras
 static DynamixelManager* manager = new DynamixelManager(&Serial1);
 static XL430* motor1 = new XL430(1,*manager);
 static XL430* motor2 = new XL430(2,*manager);
@@ -13,17 +15,31 @@ static XL430* motor3 = new XL430(3,*manager);
 
 static std::vector<XL430*> motors{motor1,motor2,motor3};
 
+
+// Pins pour
+const uint8_t DIR_PIN = 4; // Direction
+const uint8_t STEP_PIN = 2; // Vitesse
+const uint8_t RST_PIN = 3; //gris
+const unsigned int ELEVATOR_TEMPO = 800; //gris
+
+
 void setup()
 {
     pinMode(13,OUTPUT);
 
-    //delay(10000);
     Serial.begin(115200);
     Serial.println("Init");
 
-    digitalWrite(13,HIGH);
+    digitalWrite(13,HIGH); // led de débug
 
-    Serial.println(motor1->toggleTorque(true));
+    pinMode(DIR_PIN, OUTPUT);
+    pinMode(STEP_PIN, OUTPUT);
+    pinMode(RST_PIN, OUTPUT);
+    Serial.begin(9600);
+    digitalWrite(RST_PIN, HIGH);
+
+    // les moteurs fournissent du couple
+    motor1->toggleTorque(true);
     motor2->toggleTorque(true);
     motor3->toggleTorque(true);
 
@@ -33,6 +49,8 @@ void setup()
     digitalWrite(13,LOW);
 }
 
+
+// Permet de positionner le bras dans une position donnée
 void setpos(float firstMotor, float secondMotor, float thirdMotor)
 {
     Serial.println(motors.at(0)->setGoalAngle(firstMotor));
@@ -42,20 +60,35 @@ void setpos(float firstMotor, float secondMotor, float thirdMotor)
     Serial.println(motors.at(2)->setGoalAngle(thirdMotor));
 }
 
+// Commande de l'ascenseur
+void cmdAscenseur(int nbPas)
+{
+    if (nbPas < 0)
+    {
+        digitalWrite(DIR_PIN,LOW);
+    }
+    else
+    {
+        digitalWrite(DIR_PIN, HIGH);
+    }
+    for (int i = 0; i<abs(nbPas); i++)
+    {
+        digitalWrite(STEP_PIN, HIGH);
+        delayMicroseconds(ELEVATOR_TEMPO);
+        digitalWrite(STEP_PIN, LOW);
+        delayMicroseconds(ELEVATOR_TEMPO);
+    }
+}
+
 void loop()
 {
-    static float angle = 0;
-    static float oldAngle = 0;
-
-    int id = 1;
-
     if(Serial.available())
     {
-
-        oldAngle = angle;
-        String input = Serial.readString(100).toLowerCase();
+        String input = Serial.readString(100).toLowerCase(); // lit une commande
         digitalWrite(13, HIGH);
 
+
+        // Commandes du bras
         if(input == "droit")
         {
             setpos(180, 180, 180);
@@ -81,20 +114,26 @@ void loop()
             setpos(225-90, 270, 225-90);
             Serial.println("ok");
         }
+
+        // Toggle du couple fourni par les moteurs
         else if(input == "toff")
         {
-            for(unsigned int i = 0;i<3;i++) {
+            for(unsigned int i = 0;i<3;i++)
+            {
                 motors.at(i)->toggleTorque(false);
             }
             Serial.println("ok");
         }
         else if(input == "ton")
         {
-            for(unsigned int i = 0;i<3;i++) {
+            for(unsigned int i = 0;i<3;i++)
+            {
                 motors.at(i)->toggleTorque(true);
             }
             Serial.println("ok");
         }
+
+        // Donne les angles des moteurs à un moment donné
         else if(input == "output")
         {
             Serial.println("--------------------------------");
@@ -109,6 +148,15 @@ void loop()
             Serial.println("--------------------------------");
             Serial.println("ok");
         }
+
+        // Commande de l'ascenseur
+        else if(input.startsWith("asc"))
+        {
+            int nbPas = input.substring(3).toInt();
+            cmdAscenseur(nbPas);
+        }
+
+
         else
         {
            Serial.printf("Commande non reconnue '%s'\n", input);
@@ -118,40 +166,3 @@ void loop()
     delay(100);
     digitalWrite(13, LOW);
 }
-
-/*
-DynamixelPacket* XL430::makeReadPacket(DynamixelAccessData accessData)
-{
-    uint8_t packetSize = dynamixelV2::minPacketLenght+2;
-    unsigned char* packet = new unsigned char[packetSize];
-    unsigned int position = 0;
-    // HEADER
-    for(unsigned char headerPart : v2Header) {
-        packet[position++] = headerPart;
-    }
-
-    // Packet ID
-    packet[position++] = motorData.motorID;
-    motorID = motorData.motorID;
-
-    // Instruction Length: 4(params) +3
-    packet[position++] = 7;
-    packet[position++] = 0;
-
-    // Instruction
-    packet[position++] = dynamixelV2::readInstruction;
-    // Params: Start, Length
-    packet[position++] = accessData.address[0];
-    packet[position++] = accessData.address[1];
-
-    packet[position++] = accessData.length & 0xFF;
-    packet[position++] = (accessData.length >> 8) & 0xFF;
-
-    unsigned short crc = crc_compute(packet, packetSize-2);
-    packet[position++] = crc & 0xFF;
-    packet[position++] = (crc >> 8) & 0xFF;
-
-    return(new DynamixelPacket(packet, packetSize, 11+accessData.length));
-}
-
- */
