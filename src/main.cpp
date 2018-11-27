c882f0c901d70f2b3ce8b1c5aa4729d077e6cef2
jglrxavpok <jglrxavpok@gmail.com>
2018-11-27 22:35:26 +0100

    Nouvelles valeurs d'angle de test pour compenser la légère diff de hauteur avec le nouveau support pour bras

--- src/main.cpp
+++ src/main.cpp
@@ -28,8 +28,8 @@ static SyncWrite* syncWriteData = new SyncWrite(*manager, 3, (uint16_t ) (XL430:
 static float positionDroit[3] = {180.0f,180.0f,180.0f};
 static float positionAccelerateur[3] = {186.0f, 55.0f, 141.0f};
 static float positionDistributeur[3] = {234.0f, 56.0f, 93.0f};
-static float positionStockage[3] = {187.0f, 268.0f, 83.0f};
-static float positionSol[3] = {272.0f, 97.0f, 189.0f};
+static float positionStockage[3] = {/*Valeur de test:*/184.0f/*187.0f*/, 268.0f, 83.0f};
+static float positionSol[3] = { /*Valeur de test:*/ 278.0f /*272.0f*/, 97.0f, 189.0f};
 static float positionIntermediaire[3] = {208.0f, 265.0f, 88.0f};
 
 // Pins pour l'ascenseur
